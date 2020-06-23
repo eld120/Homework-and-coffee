38832c342d50af64678cc4584b64f22adc411251
[eld120] <[seyamack@gmail.com]>
2020-06-22 19:37:23 -0500

    initial setup

---
+++ caesar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+#include <string.h>
+    //mainArg, mainChk
+
+   // int key;
+
+   // char cipher[];
+
+int main(int argc, string argv[1])
+{
+    if(argc != 2 && argv[1] >= 0)
+    {
+        printf("Usage: ./caesar key");
+
+    } else
+    {
+        string plainTxt = get_string("Plain text: ");
+    }
+
+
+    printf("%s", argv[1]);
+}
