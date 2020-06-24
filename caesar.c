36d99b8be814e63cd294734adc7755ca6a48c8e3
[eld120] <[seyamack@gmail.com]>
2020-06-23 23:04:04 -0500

    completed caesar.c hw assignment

--- caesar.c
+++ caesar.c
@@ -1,24 +1,79 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-    //mainArg, mainChk
 
-   // int key;
 
-   // char cipher[];
+int key;
+int j;
+int i;
+int k;
+int temp;
+char cipher[300];
+string str;
+string plainTxt;
 
+//determine that the correct arguments are passed throught he main function
 int main(int argc, string argv[1])
 {
-    if(argc != 2 && argv[1] >= 0)
+    if (argc != 2)
     {
         printf("Usage: ./caesar key");
-
-    } else
+        return 1;
+    }
+    else if (argv[1] <= 0)
     {
-        string plainTxt = get_string("Plain text: ");
+        printf("Usage: ./caesar key");
+        return 1;
+    }
+    str = argv[1];
+
+    for (k = 0; str[k]; k++)
+    {
+        if (isdigit(str[k]))
+        {
+
+
+        }
+        else
+        {
+            printf("Please use an integar as a key!\n");
+            return 1;
+        }
+    }
+    {
+        plainTxt = get_string("Plain text: ");
     }
 
 
-    printf("%s", argv[1]);
+    key = atoi(argv[1]);
+
+    //cipertext generation
+    for (i = 0; plainTxt[i] != '\0'; i++)
+    {
+        if (plainTxt[i] >= 'a' && plainTxt[i] <= 'z')
+        {
+            temp = (plainTxt[i] + key - 97) % 26;
+            cipher[i] = temp + 97;
+            //printf("%i" , cipher[i]);
+        }
+        else if (plainTxt[i] >= 'A' && plainTxt[i] <= 'Z')
+        {
+            temp = (plainTxt[i] + key - 65) % 26;
+            cipher[i] = temp + 65;
+            //printf("%i" , cipher[i]);
+        }
+        else
+        {
+            cipher[i] = plainTxt[i] + 0;
+            printf("%i", plainTxt[i]);
+        }
+
+    }
+
+    printf("ciphertext: %s\n", cipher);
+    return 0;
+
+
 }
